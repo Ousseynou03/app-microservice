b4e73165903c017ba12f84031d3fc53bacc0ed40
Dione <dioneousseynou03@gmail.com>
2022-12-11 00:55:34 +0100

    V1.0

--- src/main/java/org/sid/inventoryservice/InventoryServiceApplication.java
+++ src/main/java/org/sid/inventoryservice/InventoryServiceApplication.java
@@ -1,13 +1,32 @@
 package org.sid.inventoryservice;
 
+import org.sid.inventoryservice.entities.InventoryService;
+import org.sid.inventoryservice.repository.InventoryServiceRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class InventoryServiceApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(InventoryServiceApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner start(InventoryServiceRepository inventoryServiceRepository){
+
+        return args -> {
+
+            inventoryServiceRepository.save(new InventoryService(null,"Computer Desk Top HP",900));
+            inventoryServiceRepository.save(new InventoryService(null,"Printer Epson",80));
+            inventoryServiceRepository.save(new InventoryService(null,"MacBook Pro Lap Top",1800));
+            inventoryServiceRepository.findAll().forEach(System.out::println);
+
+        };
+
     }
 
 }
