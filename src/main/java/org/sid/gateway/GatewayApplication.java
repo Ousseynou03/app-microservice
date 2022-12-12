71f52f9646bc998a37eb3d70fffa932ea61a891e
Dione <dioneousseynou03@gmail.com>
2022-12-12 16:00:02 +0100

    V1.2

--- src/main/java/org/sid/gateway/GatewayApplication.java
+++ src/main/java/org/sid/gateway/GatewayApplication.java
@@ -2,6 +2,9 @@ package org.sid.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.ReactiveDiscoveryClient;
+import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
+import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
@@ -13,12 +16,18 @@ public class GatewayApplication {
 
         SpringApplication.run(GatewayApplication.class, args);
     }
-    @Bean
+    //@Bean
     RouteLocator gatewayRoutes(RouteLocatorBuilder builder){
         return builder.routes().
-                route(r->r.path("/customers/**").uri("http://localhost:8081/")).
-                route(r->r.path("/inventoryServices/**").uri("http://localhost:8082/**")).
+                route(r->r.path("/customers/**").uri("lb://CUSTOMER-SERVICE")).
+                route(r->r.path("/inventoryServices/**").uri("lb://INVENTORY-SERVICE")).
                 build();
+    }
+
+    //Un nouveau bean qui prendra en compte seulement le nom des microservices
+    @Bean
+    DiscoveryClientRouteDefinitionLocator dynamicRoutes(ReactiveDiscoveryClient rdc, DiscoveryLocatorProperties dlp){
+        return new DiscoveryClientRouteDefinitionLocator(rdc, dlp);
     }
 
 }
