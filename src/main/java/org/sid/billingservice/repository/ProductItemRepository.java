e058e9af15cec4e857a94abea2183a434c275436
Dione <dioneousseynou03@gmail.com>
2022-12-14 13:15:30 +0100

    V1.1

--- src/main/java/org/sid/billingservice/repository/ProductItemRepository.java
+++ src/main/java/org/sid/billingservice/repository/ProductItemRepository.java
@@ -3,8 +3,8 @@ package org.sid.billingservice.repository;
 import org.sid.billingservice.entities.ProductItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
+
 
 public interface ProductItemRepository extends JpaRepository<ProductItem, Long> {
-    List<ProductItem> findProductItemById(Long billID);
+
 }
